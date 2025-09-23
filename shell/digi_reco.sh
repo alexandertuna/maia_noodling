71bc54526cc7e14c3f9863f93caa28c9e0b6d2df
alexandertuna <alexander.tuna@gmail.com>
2025-09-23 10:35:38 -0700

    EnableIP, lets do it

--- shell/digi_reco.sh
+++ shell/digi_reco.sh
@@ -2,7 +2,7 @@ CODE=/ceph/users/atuna/work/maia
 DATA=/ceph/users/atuna/work/maia/data
 ONE="test"
 TWO="test_process"
-NEV=20
+NEV=10
 # mkdir -p ${DATA}/recoBIB/${TWO}
 # mkdir -p ${DATA}/IPairs
 # mkdir -p ${DATA}/BIB10TeV/sim_mm_pruned
@@ -10,5 +10,5 @@ NEV=20
 # mkdir -p ${DATA}/sim/${TWO}
 
 cp ${CODE}/SteeringMacros/k4Reco/steer_reco.py steer_reco.py
-k4run steer_reco.py -n ${NEV} --TypeEvent ${TWO} --InFileName ${ONE} --enableBIB --skipReco --code ${CODE} --data ${DATA}
+k4run steer_reco.py -n ${NEV} --TypeEvent ${TWO} --InFileName ${ONE} --enableBIB --enableIP --skipReco --code ${CODE} --data ${DATA}
 # --enableIP
