b57e2697d544d8c5d5245f4a5e73cc888973cd83
alexandertuna <alexander.tuna@gmail.com>
2025-09-25 12:13:47 -0700

    Make executable, use command line to specify which input file, and use BIB steering instead of GEN

--- shell/simulate_ipairs.sh
+++ shell/simulate_ipairs.sh
@@ -1,7 +1,14 @@
-INPUT="/ceph/users/atuna/work/maia/data/IPairs/gen/pairs_cycle1.slcio"
-STEER="/ceph/users/atuna/work/maia/SteeringMacros/Sim/sim_steer_GEN_CONDOR.py"
+#!/usr/bin/env bash
+set -eo pipefail
+
+NUM=${1}
+INPUT="/ceph/users/atuna/work/maia/data/IPairs/gen/pairs_cycle${NUM}.slcio"
+# STEER="/ceph/users/atuna/work/maia/SteeringMacros/Sim/sim_steer_GEN_CONDOR.py"
+STEER="/ceph/users/atuna/work/maia/SteeringMacros/Sim/sim_steer_BIB_CONDOR.py"
 COMPACT="/ceph/users/atuna/work/maia/detector-simulation/geometries/MAIA_v0/MAIA_v0.xml"
-OUTPUT="sim_pairs_cycle1.slcio"
+OUTPUT="sim_pairs_cycle${NUM}.slcio"
+
+echo $OUTPUT
 
 rm -f ${OUTPUT}
 ddsim --inputFile ${INPUT} \
