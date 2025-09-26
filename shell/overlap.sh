d610c55d60e30a023b377d8ed94f7c3a55e7daeb
alexandertuna <alexander.tuna@gmail.com>
2025-09-26 09:20:06 -0700

    Add link

--- shell/overlap.sh
+++ shell/overlap.sh
@@ -1,3 +1,5 @@
+# https://indico.desy.de/event/36779/contributions/131996/attachments/78657/102162/221128_soft_tutorial_dd4hep.pdf
+
 CODE=/ceph/users/atuna/work/maia
 COMPACT=${CODE}/detector-simulation/geometries/MAIA_v0/MAIA_v0.xml
 MACRO=${CODE}/maia_noodling/macros/overlap.mac
@@ -6,4 +8,4 @@ NOW=$(date +%Y_%m_%d_%Hh%Mm%Ss)
 time ddsim \
      --compactFile ${COMPACT} \
      --runType run \
-     --macroFile ${MACRO} > overlap_dump_${NOW}.txt
+     --macroFile ${MACRO} 2>&1 > overlap_dump_${NOW}.txt
