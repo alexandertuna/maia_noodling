d0613ebeb542fd3a8cfb3f19ab783196252666d3
alexandertuna <alexander.tuna@gmail.com>
2025-09-30 09:48:56 -0700

    Using CODE and EVENTS

--- shell/sim.sh
+++ shell/sim.sh
@@ -1,10 +1,14 @@
-# INPUT="/ceph/users/atuna/work/maia/data/muonGun_pT_0_50_gen.slcio"
-INPUT="/ceph/users/atuna/work/maia/run/output_gen.slcio"
-STEER="/ceph/users/atuna/work/maia/SteeringMacros/Sim/sim_steer_GEN_CONDOR.py"
-COMPACT="/ceph/users/atuna/work/maia/detector-simulation/geometries/MAIA_v0/MAIA_v0.xml"
+CODE=/ceph/users/atuna/work/maia
+
+# INPUT="${CODE}/data/muonGun_pT_0_50_gen.slcio"
+INPUT="output_gen.slcio"
+STEER=${CODE}/SteeringMacros/Sim/sim_steer_GEN_CONDOR.py
+COMPACT=${CODE}/detector-simulation/geometries/MAIA_v0/MAIA_v0.xml
+EVENTS=100
 
 rm -f output_sim.slcio
 ddsim --inputFile ${INPUT} \
       --steeringFile ${STEER} \
       --compactFile ${COMPACT} \
+      --numberOfEvents ${EVENTS} \
       --outputFile output_sim.slcio
