c225e6ed2bad3189ffc3ad53edefb61320bedb4c
alexandertuna <alexander.tuna@gmail.com>
2025-09-30 09:47:58 -0700

    Playing with digi/reco

--- shell/digi_reco.sh
+++ shell/digi_reco.sh
@@ -3,12 +3,11 @@ DATA=/ceph/users/atuna/work/maia/data
 ONE="test"
 TWO="test_process"
 NEV=10
-# mkdir -p ${DATA}/recoBIB/${TWO}
-# mkdir -p ${DATA}/IPairs
-# mkdir -p ${DATA}/BIB10TeV/sim_mm_pruned
-# mkdir -p ${DATA}/BIB10TeV/sim_mp_pruned
-# mkdir -p ${DATA}/sim/${TWO}
+NOW=$(date +%Y_%m_%d_%Hh%Mm%Ss)
+LOG=log_${NOW}.txt
 
 cp ${CODE}/SteeringMacros/k4Reco/steer_reco.py steer_reco.py
-k4run steer_reco.py -n ${NEV} --TypeEvent ${TWO} --InFileName ${ONE} --enableBIB --enableIP --skipReco --skipTrackerConing --code ${CODE} --data ${DATA}
+k4run steer_reco.py -n ${NEV} --TypeEvent ${TWO} --InFileName ${ONE} --enableBIB --enableIP --skipReco --skipTrackerConing --code ${CODE} --data ${DATA} | tee ${LOG}
 # --enableIP
+
+mv ${DATA}/recoBIB/test_process/test_process_reco_test.slcio ./test_process_reco_test.slcio__IP_BIB1666
