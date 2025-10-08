d270e97a544b17b2a27b44b3597e1a5f6251de5a
alexandertuna <alexander.tuna@gmail.com>
2025-10-08 09:55:52 -0700

    Use CODE/DATA paradigm, and use k4geo

--- shell/simulate_ipairs.sh
+++ shell/simulate_ipairs.sh
@@ -2,10 +2,16 @@
 set -eo pipefail
 
 NUM=${1}
-INPUT="/ceph/users/atuna/work/maia/data/IPairs/gen/pairs_cycle${NUM}.slcio"
-# STEER="/ceph/users/atuna/work/maia/SteeringMacros/Sim/sim_steer_GEN_CONDOR.py"
-STEER="/ceph/users/atuna/work/maia/SteeringMacros/Sim/sim_steer_BIB_CONDOR.py"
-COMPACT="/ceph/users/atuna/work/maia/detector-simulation/geometries/MAIA_v0/MAIA_v0.xml"
+
+CODE="/ceph/users/atuna/work/maia"
+DATA="/ceph/users/atuna/work/maia/data"
+
+# STEER="${CODE}/SteeringMacros/Sim/sim_steer_GEN_CONDOR.py"
+# COMPACT="${CODE}/detector-simulation/geometries/MAIA_v0/MAIA_v0.xml"
+
+INPUT="${DATA}/IPairs/gen/pairs_cycle${NUM}.slcio"
+STEER="${CODE}/SteeringMacros/Sim/sim_steer_BIB_CONDOR.py"
+COMPACT="${CODE}/k4geo/MuColl/MAIA/compact/MAIA_v0/MAIA_v0.xml"
 OUTPUT="sim_pairs_cycle${NUM}.slcio"
 
 echo $OUTPUT
