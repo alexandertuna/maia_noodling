66568295c1afb1ae43a428494ccd00d5483baa36
alexandertuna <alexander.tuna@gmail.com>
2025-10-09 13:58:08 -0700

    Move pruned stuff too

--- experiments/simulate_bib.2025_10_08_09h35m49s/mv.sh
+++ experiments/simulate_bib.2025_10_08_09h35m49s/mv.sh
@@ -1,6 +1,7 @@
 TOTAL=6666
 for NUM in $(seq 1 ${TOTAL}); do
     for MUON in mm mp; do
-        mv ${MUON}_BIB_sim_${NUM}.slcio BIB10TeV/sim_${MUON}/BIB_sim_${NUM}.slcio
+        # mv ${MUON}_BIB_sim_${NUM}.slcio BIB10TeV/sim_${MUON}/BIB_sim_${NUM}.slcio
+        mv ${MUON}_pruned_BIB_sim_${NUM}.slcio BIB10TeV/sim_${MUON}_pruned/BIB_sim_${NUM}.slcio
     done
 done
