d9c124cb3349be15449a5cf25c94d1599a98ce06
alexandertuna <alexander.tuna@gmail.com>
2025-10-10 14:57:51 -0700

    De-scriptifying the muongun bibbing

---
+++ shell/digitize_muons2.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+# paths
+CODE=/ceph/users/atuna/work/maia
+DATA=/ceph/users/atuna/work/maia/maia_noodling/experiments/simulate_bib.2025_10_08_09h35m49s
+
+# env
+# it would be cool if setup_mucoll existed out-of-the-box
+# setup_mucoll
+source /opt/spack/opt/spack/__spack_path_placeholder__/__spack_path_placeholder__/__spack_path_placeholder__/__spack_path_placeholder__/linux-x86_64/mucoll-stack-master-2wtmg3ohr26uckseodhqjfjaw7mijwil/setup.sh
+export MARLIN_DLL=$(readlink -e ${CODE}/MyBIBUtils/build/lib/libMyBIBUtils.so):${MARLIN_DLL}
+
+# run
+time python ${CODE}/maia_noodling/python/digitize_muons.py --gen --sim --digi --num ${1} --data ${DATA}
