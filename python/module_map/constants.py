2089ce08eba34a38107ff31d21e47ad2d811ad65
alexandertuna <alexander.tuna@gmail.com>
2025-11-07 23:22:41 -0800

    Adding detector constants

--- python/module_map/constants.py
+++ python/module_map/constants.py
@@ -14,9 +14,23 @@ TRACKER_RELATIONS = [
     "OETrackerHitsRelations",
 ]
 
-VXD_Barrel = 1
-VXD_Endcap = 2
-IT_Barrel = 3
-IT_Endcap = 4
-OT_Barrel = 5
-OT_Endcap = 6
+VXD_BARREL = 1
+VXD_ENDCAP = 2
+IT_BARREL = 3
+IT_ENDCAP = 4
+OT_BARREL = 5
+OT_ENDCAP = 6
+
+DET_IDS = [
+    IT_BARREL,
+    IT_ENDCAP,
+    OT_BARREL,
+    OT_ENDCAP,
+]
+
+DET_NAMES = {
+    IT_BARREL: "IT Barrel",
+    IT_ENDCAP: "IT Endcap",
+    OT_BARREL: "OT Barrel",
+    OT_ENDCAP: "OT Endcap",
+}
