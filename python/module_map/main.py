a656ae1bf62df45e55586492ab38671aedf786c7
alexandertuna <alexander.tuna@gmail.com>
2025-11-10 14:46:25 -0800

    Option to load from intermediate parquet file, instead of reprocessing data from slcio

--- python/module_map/main.py
+++ python/module_map/main.py
@@ -13,6 +13,10 @@ def options():
     parser = argparse.ArgumentParser(usage=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-i", type=str, default=FNAME,
                         help="Comma-separated list of globbable input slcio files")
+    parser.add_argument("--parquet", type=str, default="hits_dataframe.parquet",
+                        help="Intermediate parquet file for hits dataframe")
+    parser.add_argument("--load_parquet", action="store_true",
+                        help="Load hits dataframe from parquet file")
     parser.add_argument("--barrel_only", action="store_true",
                         help="Consider only barrel hits for module mapping")
     return parser.parse_args()
@@ -24,14 +28,18 @@ def main():
     for fpath in file_paths:
         print(f"Input file: {fpath}")
 
-    converter = SlcioToHitsDataFrame(file_paths, barrel_only=ops.barrel_only)
-    hits_df = converter.convert()
+    if ops.load_parquet:
+        hits_df = pd.read_parquet(ops.parquet)
+    else:
+        converter = SlcioToHitsDataFrame(file_paths, barrel_only=ops.barrel_only)
+        hits_df = converter.convert()
+        print(f"Writing hits to {ops.parquet} ...")
+        hits_df.to_parquet(ops.parquet)
     print(hits_df)
     # with pd.option_context("display.min_rows", 100,
     #                        "display.max_rows", 100,
     #                        ):
     #     print(hits_df)
-    hits_df.to_parquet("hits_dataframe.parquet")
 
     module_mapper = HitsToModuleMap(hits_df)
     module_map_df = module_mapper.make_module_map()
