0097fe06d944094a19c9a265f615ed21561f8b85
alexandertuna <alexander.tuna@gmail.com>
2025-11-09 18:25:28 -0800

    Allow input arg to be globbable, add the module mapper, add more plots, and add a barrel-only option

--- python/module_map/main.py
+++ python/module_map/main.py
@@ -1,6 +1,9 @@
 import argparse
 import pandas as pd
+from glob import glob
+
 from slcio_to_hits_dataframe import SlcioToHitsDataFrame
+from hits_to_module_map import HitsToModuleMap
 from plotter import Plotter
 
 # FNAME = "/ceph/users/atuna/work/maia/maia_noodling/samples/v00/muonGun_pT_0_10_nobib/muonGun_pT_0_10_digi_0.slcio"
@@ -9,13 +12,19 @@ FNAME = "/ceph/users/atuna/work/maia/maia_noodling/experiments/simulate_muonGun.
 def options():
     parser = argparse.ArgumentParser(usage=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-i", type=str, default=FNAME,
-                        help="Comma-separated list of input slcio files")
+                        help="Comma-separated list of globbable input slcio files")
+    parser.add_argument("--barrel_only", action="store_true",
+                        help="Consider only barrel hits for module mapping")
     return parser.parse_args()
 
 
 def main():
     ops = options()
-    converter = SlcioToHitsDataFrame(ops.i.split(","))
+    file_paths = get_files(ops.i)
+    for fpath in file_paths:
+        print(f"Input file: {fpath}")
+
+    converter = SlcioToHitsDataFrame(file_paths, barrel_only=ops.barrel_only)
     hits_df = converter.convert()
     print(hits_df)
     # with pd.option_context("display.min_rows", 100,
@@ -24,8 +33,18 @@ def main():
     #     print(hits_df)
     hits_df.to_parquet("hits_dataframe.parquet")
 
-    plotter = Plotter(hits_df)
+    module_mapper = HitsToModuleMap(hits_df)
+    module_map_df = module_mapper.make_module_map()
+    print(module_map_df)
+
+    plotter = Plotter(hits_df, module_map_df)
     plotter.plot("plots.pdf")
+
+def get_files(pattern: str) -> list[str]:
+    files = []
+    for fi in pattern.split(","):
+        files.extend(glob(fi))
+    return files
 
 
 if __name__ == "__main__":
