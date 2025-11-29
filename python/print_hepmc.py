d072c9ac6fae54270c3c6d0bd2427b9bfbe2327d
alexandertuna <alexander.tuna@gmail.com>
2025-11-28 16:31:12 -0800

    Draw hepmc particle graph if requested

--- python/print_hepmc.py
+++ python/print_hepmc.py
@@ -1,12 +1,15 @@
 import argparse
+import time
 import pyhepmc
+from pyhepmc.view import savefig
 
-# FNAME = "/ceph/users/atuna/work/maia/maia_noodling/experiments/simulate_hbb.2025_10_23_16h35m00s/mumu_H_bb_10TeV.hepmc"
+NOW = time.strftime("%Y_%m_%d_%Hh%Mm%Ss")
 
 def arguments():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-i", type=str, help="Input hepmc file")
     parser.add_argument("-n", type=int, help="Number of events to process")
+    parser.add_argument("-d", action="store_true", help="Draw events to pdf")
     return parser.parse_args()
 
 def main():
@@ -18,6 +21,8 @@ def main():
     with pyhepmc.open(args.i) as f:
         print(f)
         for i, event in enumerate(f):
+            if args.d:
+                savefig(event, f"hepmc_{NOW}_event{i:04}.pdf")
             print(event)
             if args.n and i >= args.n - 1:
                 break
