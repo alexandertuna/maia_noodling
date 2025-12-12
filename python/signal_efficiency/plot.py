e72dc19e2e36ba39b10b93238881d1325c40c02e
alexandertuna <alexander.tuna@gmail.com>
2025-12-12 13:15:57 -0800

    Add plots of eta/phi too

--- python/signal_efficiency/plot.py
+++ python/signal_efficiency/plot.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
@@ -15,22 +16,71 @@ class Plotter:
     def plot(self):
         with PdfPages(self.pdf) as pdf:
             self.plot_sim_pt(pdf)
+            self.plot_sim_eta(pdf)
+            self.plot_sim_phi(pdf)
 
 
     def plot_sim_pt(self, pdf: PdfPages):
         mask = ~(self.df["hit"].astype(bool))
-        bins = 100
+        bins = np.linspace(0, 10, 101)
         fig, ax = plt.subplots(figsize=(8,8))
         ax.hist(self.df[mask]["sim_pt"],
                 bins=bins,
                 histtype="stepfilled",
-                color="blue",
-                alpha=0.5,
+                color="dodgerblue",
+                alpha=0.9,
                 label="All hits",
                 )
         ax.tick_params(direction="in", which="both", top=True, right=True)
-        ax.set_xlabel("Simulated $p_T$ [GeV/c]")
+        ax.set_xlabel("Simulated $p_T$ [GeV]")
         ax.set_ylabel("Counts")
+        ax.minorticks_on()
+        ax.grid(which="both")
+        ax.set_axisbelow(True)
+        fig.subplots_adjust(left=0.15, right=0.95, top=0.95, bottom=0.09)
+        pdf.savefig()
+        plt.close()
+
+
+    def plot_sim_eta(self, pdf: PdfPages):
+        mask = ~(self.df["hit"].astype(bool))
+        bins = np.linspace(-2, 2, 101)
+        fig, ax = plt.subplots(figsize=(8,8))
+        ax.hist(self.df[mask]["sim_eta"],
+                bins=bins,
+                histtype="stepfilled",
+                color="dodgerblue",
+                alpha=0.9,
+                label="All hits",
+                )
+        ax.tick_params(direction="in", which="both", top=True, right=True)
+        ax.set_xlabel("Simulated eta")
+        ax.set_ylabel("Counts")
+        ax.minorticks_on()
+        ax.grid(which="both")
+        ax.set_axisbelow(True)
+        fig.subplots_adjust(left=0.15, right=0.95, top=0.95, bottom=0.09)
+        pdf.savefig()
+        plt.close()
+
+
+    def plot_sim_phi(self, pdf: PdfPages):
+        mask = ~(self.df["hit"].astype(bool))
+        bins = np.linspace(-3.2, 3.2, 161)
+        fig, ax = plt.subplots(figsize=(8,8))
+        ax.hist(self.df[mask]["sim_phi"],
+                bins=bins,
+                histtype="stepfilled",
+                color="dodgerblue",
+                alpha=0.9,
+                label="All hits",
+                )
+        ax.tick_params(direction="in", which="both", top=True, right=True)
+        ax.set_xlabel("Simulated phi")
+        ax.set_ylabel("Counts")
+        ax.minorticks_on()
+        ax.grid(which="both")
+        ax.set_axisbelow(True)
         fig.subplots_adjust(left=0.15, right=0.95, top=0.95, bottom=0.09)
         pdf.savefig()
         plt.close()
