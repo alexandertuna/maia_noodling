ec08c60d28ded54185d1c7f4ff48010130b3cc94
alexandertuna <alexander.tuna@gmail.com>
2025-12-16 21:12:27 -0800

    Hinting toward the future container

--- shell/container.sh
+++ shell/container.sh
@@ -1,5 +1,6 @@
 # apptainer run /cvmfs/unpacked.cern.ch/ghcr.io/muoncollidersoft/mucoll-sim-alma9\:v2.9.7
 apptainer run /cvmfs/unpacked.cern.ch/ghcr.io/muoncollidersoft/mucoll-sim-alma9\:v2.9.8-amd64
+# apptainer run /cvmfs/unpacked.cern.ch/ghcr.io/muoncollidersoft/mucoll-sim-ubuntu24:v2.11-amd64
 
 # Tutorial image:
 # apptainer run /cvmfs/unpacked.cern.ch/gitlab-registry.cern.ch/muon-collider/mucoll-deploy/mucoll:2.9-alma9
