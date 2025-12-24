691da3173ff97c6b4e4b66c3a7a4a098efdcfcdc
alexandertuna <alexander.tuna@gmail.com>
2025-12-24 12:31:36 -0800

    Use command line args for writing to parquet and event displays. Pass geo to plotter

--- python/signal_efficiency/main.py
+++ python/signal_efficiency/main.py
@@ -18,7 +18,7 @@ FNAMES = [
     # "/ceph/users/atuna/work/maia/maia_noodling/experiments/simulate_muonGun.2025_11_06_21h31m00s/muonGun_pT_0_10_sim_4*.slcio",
 ]
 NOW = time.strftime("%Y_%m_%d_%Hh%Mm%Ss")
-PDF = f"detector_efficiency_{NOW}.pdf"
+PDF = f"detector_efficiency_NOW.pdf"
 DISPLAYS = "event_displays_NOW.pdf"
 PARQUET = f"detector_efficiency_NOW.parquet"
 PRINT_GROUPS = 0
@@ -31,8 +31,9 @@ def main():
     df = SlcioToHitsDataFrame(slcio_file_paths=fnames, load_geometry=geometry).convert()
 
     # write df to file
-    print(f"Writing data frame to {PARQUET}...")
-    df.to_parquet(PARQUET)
+    if ops.write_to_parquet:
+        print(f"Writing data frame to {PARQUET}...")
+        df.to_parquet(PARQUET)
 
     # show some info
     group_cols = ["file", "i_event", "i_mcp"]
@@ -65,8 +66,10 @@ def main():
             print(group[mask][cols].to_string(index=False))
 
     # make event displays
-    displays = EventDisplays(df, DISPLAYS)
-    displays.make_event_displays()
+    if ops.event_displays:
+        print(f"Making event displays to {DISPLAYS}...")
+        displays = EventDisplays(df, DISPLAYS)
+        displays.make_event_displays()
 
     # show the dataframe
     # with pd.option_context("display.min_rows", 50,
@@ -74,13 +77,15 @@ def main():
     #                       ):
     #     print(df)
 
-    plotter = Plotter(df, PDF)
+    plotter = Plotter(df, PDF, geometry)
     plotter.plot()
 
 
 def options():
     parser = argparse.ArgumentParser(usage=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--geometry", action="store_true", help="Load compact geometry from xml")
+    parser.add_argument("--event-displays", action="store_true", help="Make event displays")
+    parser.add_argument("--write-to-parquet", action="store_true", help="Write dataframe to parquet file")
     return parser.parse_args()
 
 
