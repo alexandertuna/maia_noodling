22f1ad708f233626a80a050bc6549c3a24a15e8e
alexandertuna <alexander.tuna@gmail.com>
2026-01-13 15:08:55 -0800

    Add v02 geo, which isnt used

---
+++ experiments/simulate_muonGun.2025_12_21_09h28m00s/go.sh
@@ -0,0 +1,11 @@
+# maia doublet v02 geometry!
+
+CODE=/ceph/users/atuna/work/maia
+# export MARLIN_DLL=$(readlink -e ${CODE}/MyBIBUtils/build/lib/libMyBIBUtils.so):${MARLIN_DLL}
+for NUM in {100..299}; do
+    time python ${CODE}/maia_noodling/python/digitize_muons.py --gen --sim --num ${NUM} --events 1000 --data $(pwd)
+    rm -f lctuple_*
+done
+
+# time python ${CODE}/maia_noodling/python/digitize_muons.py --gen --sim --digi --num 0 --events 100 --data $(pwd)
+# time python ${CODE}/maia_noodling/python/digitize_muons.py --gen --sim --digi --num 1 --events 100 --data $(pwd)
