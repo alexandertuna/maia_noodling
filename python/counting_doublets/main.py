e42cc226c26882f86204bd154942c2efb7eb05b7
alexandertuna <alexander.tuna@gmail.com>
2026-02-18 19:04:41 -0800

    Option to plot timelapse. Option to cut on doublets. Drop columns in doublets, not main. Pass signal bool to slcio

--- python/counting_doublets/main.py
+++ python/counting_doublets/main.py
@@ -14,7 +14,7 @@ from constants import SIGNAL
 
 FNAMES = [
     # neutrinoGun 100%, (-5, 15)
-    # "/ceph/users/atuna/work/maia/maia_noodling/samples/v01/neutrinoGun_n5_p15/neutrinoGun_digi_3.slcio",
+    "/ceph/users/atuna/work/maia/maia_noodling/samples/v01/neutrinoGun_n5_p15/neutrinoGun_digi_3.slcio",
 
     # neutrinoGun 10% (-5, 15)
     # "/ceph/users/atuna/work/maia/maia_noodling/samples/v01/neutrinoGun_n5_p15_0.10/neutrinoGun_digi_3.slcio",
@@ -38,16 +38,16 @@ FNAMES = [
     # "/ceph/users/atuna/work/maia/maia_noodling/samples/v01/muonGun_pT_0_10/muonGun_pT_0_10_sim_309.slcio",
 
     # muonGun, 2 GeV
-    "/ceph/users/atuna/work/maia/maia_noodling/samples/v01/muonGun_pT_2p0_2p1/muonGun_pT_2p0_2p1_sim_300.slcio",
-    "/ceph/users/atuna/work/maia/maia_noodling/samples/v01/muonGun_pT_2p0_2p1/muonGun_pT_2p0_2p1_sim_301.slcio",
-    "/ceph/users/atuna/work/maia/maia_noodling/samples/v01/muonGun_pT_2p0_2p1/muonGun_pT_2p0_2p1_sim_302.slcio",
-    "/ceph/users/atuna/work/maia/maia_noodling/samples/v01/muonGun_pT_2p0_2p1/muonGun_pT_2p0_2p1_sim_303.slcio",
-    "/ceph/users/atuna/work/maia/maia_noodling/samples/v01/muonGun_pT_2p0_2p1/muonGun_pT_2p0_2p1_sim_304.slcio",
-    "/ceph/users/atuna/work/maia/maia_noodling/samples/v01/muonGun_pT_2p0_2p1/muonGun_pT_2p0_2p1_sim_305.slcio",
-    "/ceph/users/atuna/work/maia/maia_noodling/samples/v01/muonGun_pT_2p0_2p1/muonGun_pT_2p0_2p1_sim_306.slcio",
-    "/ceph/users/atuna/work/maia/maia_noodling/samples/v01/muonGun_pT_2p0_2p1/muonGun_pT_2p0_2p1_sim_307.slcio",
-    "/ceph/users/atuna/work/maia/maia_noodling/samples/v01/muonGun_pT_2p0_2p1/muonGun_pT_2p0_2p1_sim_308.slcio",
-    "/ceph/users/atuna/work/maia/maia_noodling/samples/v01/muonGun_pT_2p0_2p1/muonGun_pT_2p0_2p1_sim_309.slcio",
+    # "/ceph/users/atuna/work/maia/maia_noodling/samples/v01/muonGun_pT_2p0_2p1/muonGun_pT_2p0_2p1_sim_300.slcio",
+    # "/ceph/users/atuna/work/maia/maia_noodling/samples/v01/muonGun_pT_2p0_2p1/muonGun_pT_2p0_2p1_sim_301.slcio",
+    # "/ceph/users/atuna/work/maia/maia_noodling/samples/v01/muonGun_pT_2p0_2p1/muonGun_pT_2p0_2p1_sim_302.slcio",
+    # "/ceph/users/atuna/work/maia/maia_noodling/samples/v01/muonGun_pT_2p0_2p1/muonGun_pT_2p0_2p1_sim_303.slcio",
+    # "/ceph/users/atuna/work/maia/maia_noodling/samples/v01/muonGun_pT_2p0_2p1/muonGun_pT_2p0_2p1_sim_304.slcio",
+    # "/ceph/users/atuna/work/maia/maia_noodling/samples/v01/muonGun_pT_2p0_2p1/muonGun_pT_2p0_2p1_sim_305.slcio",
+    # "/ceph/users/atuna/work/maia/maia_noodling/samples/v01/muonGun_pT_2p0_2p1/muonGun_pT_2p0_2p1_sim_306.slcio",
+    # "/ceph/users/atuna/work/maia/maia_noodling/samples/v01/muonGun_pT_2p0_2p1/muonGun_pT_2p0_2p1_sim_307.slcio",
+    # "/ceph/users/atuna/work/maia/maia_noodling/samples/v01/muonGun_pT_2p0_2p1/muonGun_pT_2p0_2p1_sim_308.slcio",
+    # "/ceph/users/atuna/work/maia/maia_noodling/samples/v01/muonGun_pT_2p0_2p1/muonGun_pT_2p0_2p1_sim_309.slcio",
 ]
 
 
@@ -62,26 +62,32 @@ def main():
 
     # convert slcio to hits dataframe
     converter = SlcioToHitsDataFrame(slcio_file_paths=fnames,
-                                     load_geometry=geometry)
+                                     load_geometry=geometry,
+                                     signal=signal)
     mcps, simhits = converter.convert()
 
     # make doublets from hits
-    doublets = DoubletMaker(signal=signal, simhits=simhits).df
-    if not signal:
-        # drop simhit_r_upper, ... for a speedup
-        logger.info("Dropping simhit_{x,y,z}_{upper,lower} columns from doublets ...")
-        doublets.drop(columns=[
-            "simhit_r_upper",
-            "simhit_r_lower",
-            "simhit_x_upper",
-            "simhit_x_lower",
-            "simhit_y_upper",
-            "simhit_y_lower",
-            "simhit_z_upper",
-            "simhit_z_lower",
-        ], inplace=True)
+    doublets = DoubletMaker(
+        signal=signal,
+        cut_doublets=ops.cut_doublets,
+        simhits=simhits,
+    ).df
+    # if not signal:
+    #     # drop simhit_r_upper, ... for a speedup
+    #     logger.info("Dropping simhit_{x,y,z}_{upper,lower} columns from doublets ...")
+    #     doublets.drop(columns=[
+    #         "simhit_r_upper",
+    #         "simhit_r_lower",
+    #         "simhit_x_upper",
+    #         "simhit_x_lower",
+    #         "simhit_y_upper",
+    #         "simhit_y_lower",
+    #         "simhit_z_upper",
+    #         "simhit_z_lower",
+    #     ], inplace=True)
 
     # plot stuff
+    logger.info("Creating plots ...")
     plotter = Plotter(
         signal=signal,
         mcps=mcps,
@@ -91,14 +97,17 @@ def main():
     )
     plotter.plot()
 
-    # logger.info("Creating timelapse gif ...")
-    # tl = Timelapse(df=df, event=0, gif="event.gif")
+    if ops.timelapse:
+        logger.info("Creating timelapse gif ...")
+        tl = Timelapse(df=simhits, event=0, gif="event.gif")
 
 
 def options():
     parser = argparse.ArgumentParser(usage=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-i", default=FNAMES, help="Input slcio file or glob pattern")
     parser.add_argument("--geometry", action="store_true", help="Load compact geometry from xml")
+    parser.add_argument("--timelapse", action="store_true", help="Create timelapse gif")
+    parser.add_argument("--cut-doublets", action="store_true", help="Cut doublets based on DZ_CUT and DR_CUT")
     return parser.parse_args()
 
 
