1745268dcefcc53a2d32d496980deb294f5a9c51
alexandertuna <alexander.tuna@gmail.com>
2026-02-20 14:24:23 -0800

    Remove commented block which has become its own function

--- python/counting_doublets/doublet.py
+++ python/counting_doublets/doublet.py
@@ -143,55 +143,6 @@ class DoubletMaker:
             if (self.signal and i_group % 1000 == 0) or not self.signal:
                 logger.info(f"Processed group {i_group}/{len(groups)}, doublet size = {size:.1f} MB, n(doublets) = {length} ...")
 
-            # lower_mask = group["simhit_layer_mod_2"] == 0
-            # upper_mask = group["simhit_layer_mod_2"] == 1
-
-            # # logger.info("Getting lower and upper hits ...")
-            # lower = group[lower_mask].rename(columns=lower_cols_rename)[lower_cols]
-            # upper = group[upper_mask].rename(columns=upper_cols_rename)[upper_cols]
-            # n_lower += len(lower)
-            # n_upper += len(upper)
-
-            # # inner join to find doublets
-            # if i_group == 0 and not self.signal:
-            #     logger.info("Joining to make doublets ...")
-            # doublets = lower.merge(upper, on=doublet_cols, how="inner")
-            # sizes[0] = int(doublets.memory_usage(deep=True).sum() * BYTE_TO_MB)
-
-            # # mention memory usage
-            # if i_group == 0 and not self.signal:
-            #     buf = io.StringIO()
-            #     doublets.info(memory_usage='deep', buf=buf)
-            #     logger.info(f"doublets.info\n{buf.getvalue()}")
-
-            # # rz
-            # if i_group == 0 and not self.signal:
-            #     logger.info("Adding derived doublet quantities ...")
-            # slope = np.divide(doublets["simhit_z_upper"] - doublets["simhit_z_lower"],
-            #                   doublets["simhit_r_upper"] - doublets["simhit_r_lower"])
-            # doublets["intercept_rz"] = doublets["simhit_z_lower"] - doublets["simhit_r_lower"] * slope
-
-            # # xy: dphi
-            # phi_local = np.arctan2(doublets["simhit_y_upper"] - doublets["simhit_y_lower"],
-            #                        doublets["simhit_x_upper"] - doublets["simhit_x_lower"])
-            # phi_global = np.arctan2((doublets["simhit_y_lower"] + doublets["simhit_y_upper"]) / 2.0,
-            #                         (doublets["simhit_x_lower"] + doublets["simhit_x_upper"]) / 2.0)
-            # doublets["dphi"] = phi_local - phi_global
-            # doublets["dphi"] = (doublets["dphi"] + np.pi) % (2 * np.pi) - np.pi
-
-            # # xy: dr at point of closest approach to origin
-            # slope_xy = np.divide(doublets["simhit_y_upper"] - doublets["simhit_y_lower"],
-            #                      doublets["simhit_x_upper"] - doublets["simhit_x_lower"])
-            # intercept_xy = doublets["simhit_y_lower"] - slope_xy * doublets["simhit_x_lower"]
-            # doublets["dr"] = np.abs(intercept_xy) / np.sqrt(1 + slope_xy**2)
-
-            # # drop columns which arent used downstream
-            # if not self.signal:
-            #     doublets.drop(columns=drop_cols, inplace=True)
-            #     sizes[1] = int(doublets.memory_usage(deep=True).sum() * BYTE_TO_MB)
-
-            # doublets_list.append(doublets)
-
         doublets = pd.concat(all_doublets, ignore_index=True)
         cutflow = pd.DataFrame(all_cutflows)
         for col in cutflow.columns:
