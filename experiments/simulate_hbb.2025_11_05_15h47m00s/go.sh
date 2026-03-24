59306dc9d3b493d33c5c1b998861c7b6f7e8953b
alexandertuna <alexander.tuna@gmail.com>
2026-03-24 16:57:53 -0700

    More hbb playing around

---
+++ experiments/simulate_hbb.2025_11_05_15h47m00s/go.sh
@@ -0,0 +1,1 @@
+time python ~/work/maia/maia_noodling/python/digitize_muons.py --gen --typeevent mumu_H_bb_10TeV --num 0
---
+++ experiments/simulate_hbb.2025_11_05_15h47m00s/reduced_stats/go.sh
@@ -0,0 +1,2 @@
+time python ~/work/maia/maia_noodling/python/digitize_muons.py --gen --sim --typeevent mumu_H_bb_10TeV --events 1 --num 0
+
