91eff9a3cbb772b2acccd39354abbc0564ca3e61
alexandertuna <alexander.tuna@gmail.com>
2026-03-24 17:22:31 -0700

    Second lamer attempt at muon gun

---
+++ experiments/simulate_muonGun.2025_12_11_00h00m00s/go.sh
@@ -0,0 +1,9 @@
+CODE=/ceph/users/atuna/work/maia
+# export MARLIN_DLL=$(readlink -e ${CODE}/MyBIBUtils/build/lib/libMyBIBUtils.so):${MARLIN_DLL}
+for NUM in {2..3}; do
+    time python digitize_muons.py --gen --sim --num ${NUM} --events 50 --data $(pwd)
+    rm -f lctuple_*
+done
+
+# time python ${CODE}/maia_noodling/python/digitize_muons.py --gen --sim --digi --num 0 --events 100 --data $(pwd)
+# time python ${CODE}/maia_noodling/python/digitize_muons.py --gen --sim --digi --num 1 --events 100 --data $(pwd)
