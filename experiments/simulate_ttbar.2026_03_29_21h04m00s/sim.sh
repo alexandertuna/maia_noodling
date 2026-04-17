e0c98a07835c10abecbcf6d14551e76ec63bc3a4
alexandertuna <alexander.tuna@gmail.com>
2026-04-17 16:21:32 -0700

    Try simulating all 100 events in chunks of 10

--- experiments/simulate_ttbar.2026_03_29_21h04m00s/sim.sh
+++ experiments/simulate_ttbar.2026_03_29_21h04m00s/sim.sh
@@ -3,11 +3,11 @@ CODE=/ceph/users/atuna/work/maia
 INPUT="events.hepmc"
 STEER=${CODE}/SteeringMacros/Sim/sim_steer_GEN_CONDOR.py
 COMPACT=${CODE}/detector-simulation/geometries/MAIA_v0/MAIA_v0.xml
-NUMBER_OF_EVENTS=1
-# SKIP_N_EVENTS=20
+NUMBER_OF_EVENTS=10
 MAX_JOBS=25
+NOW=$(date +%Y_%m_%d_%Hh%Mm%Ss)
 
-for SKIP_N_EVENTS in $(seq 0 99); do
+for SKIP_N_EVENTS in $(seq 0 ${NUMBER_OF_EVENTS} 99); do
 
     # Process at most $MAX_JOBS in parallel
     while (( $(jobs -rp | wc -l) >= MAX_JOBS )); do
@@ -19,10 +19,10 @@ for SKIP_N_EVENTS in $(seq 0 99); do
     time ddsim --inputFile ${INPUT} \
                --steeringFile ${STEER} \
                --compactFile ${COMPACT} \
-               --numberOfEvents ${EVENTS} \
+               --numberOfEvents ${NUMBER_OF_EVENTS} \
                --skipNEvents ${SKIP_N_EVENTS} \
-               --outputFile ttbar_sim_${SKIP_N_EVENTS}.slcio \
-               2>&1 | tee log_${SKIP_N_EVENTS}.txt &
+               --outputFile ttbar_sim_${NOW}_${SKIP_N_EVENTS}.slcio \
+               2>&1 | tee log_${NOW}_${SKIP_N_EVENTS}.txt &
 
     sleep 10
 
