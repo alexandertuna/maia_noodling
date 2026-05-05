88ce110350728d851fa408c738b3ab20332fbb0a
alexandertuna <alexander.tuna@gmail.com>
2026-05-05 15:00:53 -0700

    Changing tactic from merge-then-root to root-then-merge

--- experiments/simulate_ttbar.2026_04_28_19h00m00s/merge_reco.sh
+++ experiments/simulate_ttbar.2026_04_28_19h00m00s/merge_reco.sh
@@ -1,5 +1,22 @@
+# sequential version
+# for HUNNY in $(seq 0 0); do
+#     TAG=ttbar_reco_merge_${HUNNY}XX
+#     echo "Working on ${TAG} sequentially ... "
+#     time lcio_merge_files ${TAG}.slcio ttbar_reco_${HUNNY}*.slcio
+#     time lcio2edm4hep ${TAG}.slcio ${TAG}_edm4hep.root
+# done
+
+# parallel version
 for HUNNY in $(seq 0 9); do
-    TAG=ttbar_reco_merge_${HUNNY}XX
-    time lcio_merge_files ${TAG}.slcio ttbar_reco_${HUNNY}*.slcio
-    time lcio2edm4hep ${TAG}.slcio ${TAG}_edm4hep.root
+    echo "Working on ${HUNNY} ... "
+    hadd ttbar_reco_merge_${HUNNY}XX_edm4hep.root ttbar_reco_${HUNNY}*_edm4hep.root &
+    sleep 1
+    # (
+    #     TAG=ttbar_reco_merge_${HUNNY}XX
+    #     echo "Working on ${TAG} in parallel ... "
+    #     time lcio_merge_files ${TAG}.slcio ttbar_reco_${HUNNY}*.slcio
+    #     time lcio2edm4hep ${TAG}.slcio ${TAG}_edm4hep.root
+    # ) > log_${HUNNY}.txt 2>&1 &
 done
+wait
+echo "All done"
