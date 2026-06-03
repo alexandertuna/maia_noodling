bee857ff48ae4206d5fb5dd8c94dbf8e2db22411
alexandertuna <alexander.tuna@gmail.com>
2026-06-02 18:28:12 -0700

    Testing if sample production works after updating container

---
+++ experiments/simulate_muonGun.2026_06_02_18h21m00s/README.md
@@ -0,0 +1,1 @@
+Checking if my normal gen/sim/digi workflow runs after updating the mucol container from 2.9.8 to 2.11
---
+++ experiments/simulate_muonGun.2026_06_02_18h21m00s/go.sh
@@ -0,0 +1,19 @@
+MAX_JOBS=10
+CODE=/ceph/users/atuna/work/maia
+
+for NUM in {100..100}; do
+
+    # Process at most $MAX_JOBS in parallel
+    while (( $(jobs -rp | wc -l) >= MAX_JOBS )); do
+        echo "Waiting at $(date) ..."
+        sleep 1m
+    done
+
+    echo "Launching ${NUM}"
+    python digitize_muons.py --gen --sim --num ${NUM} --typeevent muonGun_pT_2p0_2p1 --events 1000 --data $(pwd) &> log_${NUM}.txt &
+
+done
+
+echo "Waiting!"
+wait
+echo "Done ^.^"
