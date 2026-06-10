411d773a018612dd14b6de26227711d5846c0f7a
alexandertuna <alexander.tuna@gmail.com>
2026-06-10 14:22:56 -0700

    neutrinoGun for v05 geo too

--- experiments/simulate_neutrinoGun.2026_06_05_14h51m00s/digitize_neutrinos.sh
+++ experiments/simulate_neutrinoGun.2026_06_05_14h51m00s/digitize_neutrinos.sh
@@ -3,7 +3,8 @@ set -eo pipefail
 
 # paths
 CODE=/ceph/users/atuna/work/maia
-DATA=${CODE}/maia_noodling/experiments/simulate_bib.2026_01_07_22h00m00s
+# DATA=${CODE}/maia_noodling/experiments/simulate_bib.2026_01_07_22h00m00s # v01
+DATA=${CODE}/maia_noodling/experiments/simulate_bib.2026_05_14_12h00m00s # v05
 TYPEEVENT="neutrinoGun"
 # RESOLUTIONUV="0.000"
 
@@ -13,7 +14,7 @@ TYPEEVENT="neutrinoGun"
 # source /opt/spack/opt/spack/__spack_path_placeholder__/__spack_path_placeholder__/__spack_path_placeholder__/__spack_path_placeholder__/linux-x86_64/mucoll-stack-master-2wtmg3ohr26uckseodhqjfjaw7mijwil/setup.sh
 # export MARLIN_DLL=$(readlink -e ${CODE}/MyBIBUtils/build/lib/libMyBIBUtils.so):${MARLIN_DLL}
 
-for RESOLUTIONUV in 0.005 0.010 0.020; do
+for RESOLUTIONUV in 0.000 0.005 0.010 0.020; do
 
     mkdir -p ${RESOLUTIONUV}
 
