0f29a91c20d8fcec39270e2f5c149644250ebb89
alexandertuna <alexander.tuna@gmail.com>
2026-07-08 10:26:02 -0700

    Continue setting up test

--- experiments/simulate_ttbar.2026_07_08_10h14m00s/gen.sh
+++ experiments/simulate_ttbar.2026_07_08_10h14m00s/gen.sh
@@ -4,7 +4,10 @@
 
 # source /cvmfs/sw.hsf.org/key4hep/setup.sh -r 2025-01-28
 
-for IT in $(seq 100 199); do
-    NEV="100" && \
-    time k4run pythia.py -n ${NEV} --Dumper.Filename ttbar_${NEV}ev_${IT}.hepmc --Pythia8.PythiaInterface.pythiacard p8_mumu_tt_ecm10000_${IT}.cmd
-done
+# for IT in $(seq 100 199); do
+#     NEV="100" && \
+#     time k4run pythia.py -n ${NEV} --Dumper.Filename ttbar_${NEV}ev_${IT}.hepmc --Pythia8.PythiaInterface.pythiacard p8_mumu_tt_ecm10000_${IT}.cmd
+# done
+
+time k4run pythia.py --Dumper.Filename ttbar.hepmc --Pythia8.PythiaInterface.pythiacard p8_mumu_tt_ecm10000.cmd
+
