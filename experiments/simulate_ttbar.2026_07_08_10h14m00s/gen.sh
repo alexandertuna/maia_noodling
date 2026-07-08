8aee0dd737b179ed51224a6a48358d3700f38c91
alexandertuna <alexander.tuna@gmail.com>
2026-07-08 10:23:06 -0700

    Starting a new dir for bigger scale ttbar

---
+++ experiments/simulate_ttbar.2026_07_08_10h14m00s/gen.sh
@@ -0,0 +1,10 @@
+
+# apptainer run -B /cvmfs/sw.hsf.org/key4hep/ /cvmfs/unpacked.cern.ch/ghcr.io/muoncollidersoft/mucoll-sim-alma9\:v2.9.8-amd64
+# $ for IT in $(seq 100 199); do cp -a p8_mumu_tt_ecm10000.cmd p8_mumu_tt_ecm10000_${IT}.cmd; sed -i "s/12345/${IT}/g" p8_mumu_tt_ecm10000_${IT}.cmd; done
+
+# source /cvmfs/sw.hsf.org/key4hep/setup.sh -r 2025-01-28
+
+for IT in $(seq 100 199); do
+    NEV="100" && \
+    time k4run pythia.py -n ${NEV} --Dumper.Filename ttbar_${NEV}ev_${IT}.hepmc --Pythia8.PythiaInterface.pythiacard p8_mumu_tt_ecm10000_${IT}.cmd
+done
