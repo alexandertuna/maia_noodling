7b5f2243bc6744c9e511a4be71e4853933aef6e9
alexandertuna <alexander.tuna@gmail.com>
2026-07-15 17:21:23 -0700

    Can I generate pions too

---
+++ experiments/simulate_pionGun.2026_07_15_17h13m00s/README.md
@@ -0,0 +1,1 @@
+Making pions with v01 and v05 geometry for pt range of 0-10 GeV (gen, sim, digi)
---
+++ experiments/simulate_pionGun.2026_07_15_17h13m00s/go.sh
@@ -0,0 +1,40 @@
+MAX_JOBS=10
+CODE=/ceph/users/atuna/work/maia
+TYPEEVENT="pionGun_pT_0_10"
+
+# env
+# it would be cool if setup_mucoll existed out-of-the-box
+# setup_mucoll
+# source /opt/spack/opt/spack/__spack_path_placeholder__/__spack_path_placeholder__/__spack_path_placeholder__/__spack_path_placeholder__/linux-x86_64/mucoll-stack-master-2wtmg3ohr26uckseodhqjfjaw7mijwil/setup.sh
+# export MARLIN_DLL=$(readlink -e ${CODE}/MyBIBUtils/build/lib/libMyBIBUtils.so):${MARLIN_DLL}
+
+for RESOLUTIONUV in 0.000 0.010; do
+
+    mkdir -p v01/${RESOLUTIONUV}
+
+    for NUM in {300..309}; do
+
+        while (( $(jobs -rp | wc -l) >= MAX_JOBS )); do
+            echo "Waiting at $(date) ..."
+            sleep 10s
+        done
+
+        time python digitize_muons.py \
+             --gen \
+             --sim \
+             --digi \
+             --num ${NUM} \
+             --typeevent ${TYPEEVENT} \
+             --events 1000 \
+             --ResolutionUV ${RESOLUTIONUV} \
+             --data $(pwd) &> ${TYPEEVENT}_log_${NUM}.txt &
+
+    done
+
+    echo "Waiting!"
+    wait
+    mv ${TYPEEVENT}_* v01/${RESOLUTIONUV}/
+
+done
+
+echo "Done ^.^"
